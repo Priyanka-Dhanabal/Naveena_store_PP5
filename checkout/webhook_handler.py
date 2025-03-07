dd2adebe9a83c3a4544e423ab432a135aedd3bc3
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2025-03-07 15:54:46 +0000

    trying to solve the issue of not receiving the confirmation email

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,12 +2,14 @@ from django.http import HttpResponse
 import stripe
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from products.models import Product
 from profiles.models import UserProfile
 import json
 import time
+import logging
 
 
 class StripeWH_Handler:
@@ -19,20 +21,29 @@ class StripeWH_Handler:
 
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
-        cust_email = order.email
-        subject = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_subject.txt',
-            {'order': order})
-        body = render_to_string(
-            'checkout/confirmation_emails/confirmation_email_body.txt',
-            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
-        
-        send_mail(
-            subject,
-            body,
-            settings.DEFAULT_FROM_EMAIL,
-            [cust_email]
-        )    
+        logger.info(f"Sending confirmation email for order {order.id}")
+        try:
+            cust_email = order.email
+            subject = render_to_string(
+                'checkout/confirmation_emails/confirmation_email_subject.txt',
+                {'order': order})
+            body = render_to_string(
+                'checkout/confirmation_emails/confirmation_email_body.txt',
+                {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+            
+            send_mail(
+                subject,
+                body,
+                settings.DEFAULT_FROM_EMAIL,
+                [cust_email]
+            )    
+
+            print(f"Confirmation email sent to {cust_email}")
+        except BadHeaderError:
+            print("Invalid header found.")
+        except Exception as e:
+            print(f"Error sending email: {str(e)}")
+        logger.info(f"Confirmation email sent to {cust_email}")
 
 
     def handle_event(self, event):
@@ -113,16 +124,16 @@ class StripeWH_Handler:
             order = None
             try:
                 order = Order.objects.create(
-                    full_name__iexact=shipping_details.name,
+                    full_name=shipping_details.name,
                     user_profile=profile,
-                    email__iexact=billing_details.email,
-                    phone_number__iexact=shipping_details.phone,
-                    country__iexact=shipping_details.address.country,
-                    postcode__iexact=shipping_details.address.postal_code,
-                    town_or_city__iexact=shipping_details.address.city,
-                    street_address1__iexact=shipping_details.address.line1,
-                    street_address2__iexact=shipping_details.address.line2,
-                    county__iexact=shipping_details.address.state,
+                    email=billing_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    town_or_city=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    county=shipping_details.address.state,
                     grand_total=grand_total,
                     original_bag=bag,
                     stripe_pid=pid,
