1d07fad8ccdc1ca7cee33511b355df851c7c7338
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2025-03-08 18:23:00 +0000

    Steps performed to deploy into heroku and postgrs sql is setup

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn naveena_store.wsgi:application
--- naveena_store/settings.py
+++ naveena_store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 if os.path.isfile('env.py'):
     import env
@@ -23,12 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*+65(oxj*!e_0!-##8p+g*2g2!*^6pfg(%1h-wsy6hh%!6a^h%'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-priyankadha-naveenastor-wc1ygv39ftc.ws-eu118.gitpod.io','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['8000-priyankadha-naveenastor-wc1ygv39ftc.ws-eu118.gitpod.io',
+                '127.0.0.1',
+                'localhost',
+                'naveenas-store-a7c4968621d6.herokuapp.com']
 
 
 # Application definition
@@ -121,13 +125,18 @@ WSGI_APPLICATION = 'naveena_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+     
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
